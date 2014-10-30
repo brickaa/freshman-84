deee13c7e57a6c474058435bd39146ca94e40b78
Becca Aaronson <becca.aaronson@gmail.com>
2014-10-30 12:05:37 -0500

    set up basic card styles, load spreadsheet variables

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,8 +17,39 @@ $(document).ready(function() {
 
     for (var i = 0; i < data.feed.entry.length; i++) {
       var model = data.feed.entry[i];
-      console.log(model.gsx$name.$t);
-      // $('<div>code</div>').appendTo('.whatever');
+      var name = model.gsx$name.$t;
+      var party = model.gsx$party.$t;
+      var chamber = model.gsx$chamber.$t;
+      var district = model.gsx$district.$t;
+      var hometown = model.gsx$hometown.$t;
+      var age = model.gsx$age.$t;
+      var bio = model.gsx$bio.$t;
+      var occupation = model.gsx$occupation.$t;
+      var education = model.gsx$education.$t;
+      var relationship = model.gsx$relationshipstatus.$t;
+      var kids = model.gsx$numberkids.$t;
+      var religion = model.gsx$religion.$t;
+
+      $('<div class="prose freshman">' +
+          '<div class="headshot">' +
+            '<img src="http://www.fillmurray.com/131/197">' +
+          '</div>' +
+          '<div class="info">' +
+            '<h3>' + name + ' (' + party + ') &mdash; HD-#</h3>' +
+            '<p>Here is a brief biography of this person. Like how they claimed Mount Everest, took over the family fridge delivery business and survive a monsoon wedding. Very informative, yet concise.</p>' +
+            '<ul>' +
+              '<li><span class="header">Hometown: </span> </li>' +
+              '<li><span class="header">Age: </span> </li>' +
+              '<li><span class="header">Family: </span> </li>' +
+            '</ul>' +
+            '<ul>' +
+              '<li><span class="header">Occupation: </span> </li>' +
+              '<li><span class="header">Education: </span> </li>' +
+              '<li><span class="header">Religion: </span> </li>' +
+            '</ul>' +
+          '</div>' +
+          '<hr class="yellow">' +
+        '</div>').appendTo('.graphic');
     }
   }
 
