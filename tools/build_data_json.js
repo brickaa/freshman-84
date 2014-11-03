b22ebb86599c708152d06eb90b1676ce892ca3c6
Becca Aaronson <becca.aaronson@gmail.com>
2014-11-03 11:47:21 -0600

    write script to build json from worksheet

--- tools/build_data_json.js
+++ tools/build_data_json.js
@@ -19,33 +19,16 @@ SHEETS.forEach(function(sheet) {
   'use strict';
 
   var worksheet = workbook.Sheets[sheet];
+  // var test = XLSX.utils.sheet_to_json(worksheet);
 
-  var temp = {};
+  // test.forEach(function(e) {
+  //   if (!e['Bio']) {
+  //     return;
+  //   }
+  //   console.log(marked(e['Bio']));
+  // });
 
-  for (var cell in worksheet) {
-    if (cell[0] === '!') { continue; }
-    if (cell[0] === 'A') {
-      var aCell = worksheet[cell];
-      aCell = aCell ? aCell.v : '';
-
-      var cellNumber = cell.match(/\d+/)[0];
-
-      var bCell = worksheet['B' + cellNumber];
-      bCell = bCell ? bCell.v : '';
-
-      var cCell = worksheet['C' + cellNumber];
-      cCell = cCell ? cCell.v : '';
-
-
-      if (cCell === 'markdown') {
-        bCell = marked(bCell);
-      }
-
-      temp[aCell] = bCell;
-    }
-  }
-
-  DATA[sheet] = temp;
+  DATA[sheet] = XLSX.utils.sheet_to_json(worksheet);
 });
 
 fs.writeFileSync('data.json', JSON.stringify(DATA, null, 2));
